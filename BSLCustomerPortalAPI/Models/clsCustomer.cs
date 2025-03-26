5dda8edfa4942d9caa8e663785a2ede20b011bbb
Kanchan <banswaraws@gmail.com>
2025-03-26 16:24:40 +0530

    Class added

---
+++ BSLCustomerPortalAPI/Models/clsCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BSLCustomerPortalAPI.Models
+{
+    public class clsCustomer
+    {
+        public Int64 CustomerId { get; set; }
+        public string CustEmailId { get; set; }
+        public string CustCompanyName { get; set; }
+        public string CustADRNR { get; set; }
+        public string CustCountryCode { get; set; }
+        public string CustCity { get; set; }
+        public string CustPostalCode { get; set; }
+        public string CustMobile { get; set; }
+        public string CreateDate { get; set; }
+        public string CustPanNo { get; set; }
+        public string CustName { get; set; }
+        public string CustPassword { get; set; }
+        public string ErrorMsg { get; set; }
+    }
+}
